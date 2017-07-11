adc8ef3702a2223f4b91c145ac5d774dff56ba14
Nmb23 <photographers@live.co.uk>
2017-07-11 20:08:15 +0100

    NB: Added 2 Second Delay and Transfer (to Loader) Method to SplashScreen.java

--- app/src/main/java/com/example/project/photoapplication/SplashScreen.java
+++ app/src/main/java/com/example/project/photoapplication/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.project.photoapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // NB : Delay 2 secs before transferring to Loader View
+     Thread timeDelay = new Thread() {
+         @Override
+         public void run() {
+             try {
+                 sleep(2000);
+                 Intent intent = new Intent(getApplicationContext(),Loader.class);
+                 startActivity(intent);
+                 finish();
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+         }
+     };
+
+        timeDelay.start();
+
     }
 }
