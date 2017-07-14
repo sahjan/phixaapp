2eb5e158fd00a32ff70ff7db0274ad9e154cdb1c
Nmb23 <photographers@live.co.uk>
2017-07-14 18:02:18 +0100

    Merge branch 'master' of D:\MSC COMPUTER SCIENCE\7) PROJECT AND DISSERTATION\APP\PhotoApplication with conflicts.

---
+++ app/src/main/java/com/example/project/photoapplication/RecentImages.java
@@ -0,0 +1,13 @@
+package com.example.project.photoapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecentImages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recent_images);
+    }
+}
