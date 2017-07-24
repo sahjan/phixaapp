82095f058096101349b8f3bd965d69fa92d61f17
Nmb23 <photographers@live.co.uk>
2017-07-24 16:03:35 +0100

    NB: Added Corresponding Tool Java and Activity XML files. Uncommented/updated in Manifest.

---
+++ app/src/main/java/com/example/project/photoapplication/Overlay3.java
@@ -0,0 +1,13 @@
+package com.example.project.photoapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Overlay3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overlay3);
+    }
+}
